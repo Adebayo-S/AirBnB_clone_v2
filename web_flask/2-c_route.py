686c963396f590415745af974e8dcaa6357f2afa
Adebayo Samuel <eladebayoor@gmail.com>
2022-10-16 22:07:56 +0100

    [feat]: file 2 added

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /:     display "Hello HBNB!"
+            /hbnb: display "HBNB"
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    """displays C followed by a formated text value"""
+    return f"C {text.replace('_', '')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
