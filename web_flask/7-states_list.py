243b6cdf4bfa7ff4cecc8b5dae15da0e9b0c54ae
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-09 04:21:37 +0100

    [fix]: used string as input

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,7 +16,7 @@ def close_session(error):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ displays HTML page with sorted list of states """
-    states = storage.all(State).values()
+    states = storage.all("State")
     states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
 
